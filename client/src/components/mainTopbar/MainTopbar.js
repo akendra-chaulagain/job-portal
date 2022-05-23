f3d7cf8dc3e1d605207cd70d7d757986c4ceede9
Akendra Chaulagain <yakendrachaulagain27@gmail.com>
2022-05-23 14:03:10 +0545

    topbar added

--- client/src/components/mainTopbar/MainTopbar.js
+++ client/src/components/mainTopbar/MainTopbar.js
@@ -10,28 +10,28 @@ const MainTopbar = () => {
           <div className="col-4 leftSideMainTopBar">
             {/* location icon */}
             <div className="locationIcon">
-              <i class="fa-solid fa-location-dot"></i>
+              <i className="fa-solid fa-location-dot"></i>
               <span>Kathmandu, Nepal</span>
             </div>
           </div>
           {/* middle part contain */}
           <div className="col-4 middleSideMainTopBar">
             <div className="phoneCallIcon">
-              <i class="fa-solid fa-phone"></i>
+              <i className="fa-solid fa-phone"></i>
               <span>+564668658</span>
             </div>
             <div className="officeTimeIcon">
-              <i class="fa-solid fa-clock"></i>
+              <i className="fa-solid fa-clock"></i>
               <span>Mon - Fri: 9:00AM - 8:00PM</span>
             </div>
           </div>
           {/* right side contain */}
           <div className="col-4 rightSidemainTopBar">
             <div className="rightSideicons">
-              <i class="fa-brands fa-facebook"></i>
-              <i class="fa-brands fa-instagram"></i>
-              <i class="fa-brands fa-twitter"></i>
-              <i class="fa-brands fa-linkedin"></i>
+              <i className="fa-brands fa-facebook"></i>
+              <i className="fa-brands fa-instagram"></i>
+              <i className="fa-brands fa-twitter"></i>
+              <i className="fa-brands fa-linkedin"></i>
             </div>
           </div>
         </div>
--- client/src/components/topbar/Topbar.js
+++ client/src/components/topbar/Topbar.js
@@ -1,7 +1,72 @@
 import React from "react";
+import "./Topbar.css";
 
 const Topbar = () => {
-  return <div>Topbar</div>;
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg ">
+        <div className="container-fluid">
+          <div className="companyLogo">
+            <img
+              className="img-fluid"
+              src="https://global-uploads.webflow.com/5e157547d6f791d34ea4e2bf/6087f7f9e5ecc6623f3ecbbe_logo-example2.svg"
+              alt="company_logo"
+            />
+          </div>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              {/* home page */}
+              <li className="nav-item dropdown">
+                <a className="nav-link dropbtn" href="/">
+                  HOME
+                </a>
+                {/* dropdown menu */}
+                <div className="dropdown-content">
+                  <a href="*">Employers</a>
+                </div>
+              </li>
+              {/* about us page */}
+              <li className="nav-item">
+                <a className="nav-link" href="/about">
+                  ABOUT US
+                </a>
+              </li>
+              {/* requitment*/}
+              <li className="nav-item ">
+                <a className="nav-link " href="/services">
+                  RECRUITMENT
+                </a>
+              </li>
+              {/* outsourcing */}
+              <li className="nav-item">
+                <a className="nav-link" href="/blogs">
+                  OUTSOURCING
+                </a>
+              </li>
+              {/* legalization */}
+              <li className="nav-item">
+                <a className="nav-link" href="/carrers">
+                  LEGALIZATION
+                </a>
+              </li>
+              {/* blogs */}
+              <li className="nav-item">
+                <a className="nav-link" href="/carrers">
+                  BLOGS
+                </a>
+              </li>
+              {/* contact */}
+              <li className="nav-item">
+                <a className="nav-link" href="/contactt">
+                  CONTACT
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 };
 
 export default Topbar;
