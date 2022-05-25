b006fed5554d489b1ceebabf14aee3ebecfc3688
Akendra Chaulagain <yakendrachaulagain27@gmail.com>
2022-05-25 13:30:57 +0545

    blog page added

--- client/src/pages/blog/Blog.js
+++ client/src/pages/blog/Blog.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Blog.css";
+import { blogData } from "./blodData";
+import Footer from "../../components/footer/Footer"
 
 const Blog = () => {
   return (
@@ -8,8 +10,27 @@ const Blog = () => {
         <div className="blogTop">
           <h3 className="text-center">Blogs</h3>
         </div>
-        <div className="row"></div>
+        <div className="row">
+          {blogData.map((item, id) => (
+            <div className="col-md-3 blogContainer" key={id}>
+              <div className="blogContainWrapper">
+                {/* board members photo */}
+                <div class="card" key={id}>
+                  <img src={item.img} class="card-img-top" alt="member_img" />
+                  <div class="card-body">
+                    {/* blog's title */}
+                    <h5 class="card-title text-center">{item.title}</h5>
+                    {/*date*/}
+                    <p class="card-text">{item.date}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
+      {/* footer */}
+      <Footer/>
     </>
   );
 };
